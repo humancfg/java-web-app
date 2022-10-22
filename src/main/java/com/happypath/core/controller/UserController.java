d39b0e5f294de98c5e1cdbafabcdd92925e8fc11
humancfg <v1adm4rkov@ya.ru>
2022-10-22 15:47:08 +0300

    refactored controller

---
+++ src/main/java/com/happypath/core/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.happypath.core.controller;
+
+import com.happypath.core.model.User;
+import com.happypath.core.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.getUser(id);
+    }
+}
