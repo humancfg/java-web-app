17b2584582d4a292b07850deb35ca18815028796
humancfg <v1adm4rkov@ya.ru>
2022-10-22 16:22:27 +0300

    added condition if user exist in DB

--- src/main/java/com/happypath/core/db/InitUsers.java
+++ src/main/java/com/happypath/core/db/InitUsers.java
@@ -3,9 +3,13 @@ package com.happypath.core.db;
 import com.happypath.core.model.User;
 import com.happypath.core.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.ignoreCase;
 
 public class InitUsers {
     @Autowired
@@ -18,7 +22,12 @@ public class InitUsers {
             User user = new User();
             user.setEmail(String.format("asd%s@mail.ru", i));
             user.setPassword(String.format("password%s", i));
-            users.add(user);
+            ExampleMatcher emailMatcher = ExampleMatcher.matching()
+                    .withIgnorePaths("id")
+                    .withMatcher("email", ignoreCase());
+            if(!userRepository.exists(Example.of(user, emailMatcher))) {
+                users.add(user);
+            }
         }
 
         userRepository.saveAll(users);
